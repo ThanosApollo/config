31c0f231a1b9654316d3378a36260e5421be8b5c
ThanosApollo <apollothanosmd@gmail.com>
2022-06-05 22:42:49 +0300

    Add more aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,7 @@ end
 fish_add_path /home/apollo/.spicetify
 alias config='/usr/bin/git --git-dir=$HOME/Developer/config/ --work-tree=$HOME'
 alias ga='git add'
+alias gaa='git add .'
 alias gc='git commit -m'
 alias gp='git push -u origin'
+alias gpm='git push -u origin master'
