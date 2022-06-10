6a6f75a22c7a7e9136e90baa9e231d6ae7219559
ThanosApollo <apollothanosmd@gmail.com>
2022-06-11 00:26:54 +0300

    Add doom emacs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+export PATH="$HOME/.emacs.d/bin:$PATH"
 fish_add_path /home/apollo/.spicetify
 alias config='/usr/bin/git --git-dir=$HOME/Developer/config/ --work-tree=$HOME'
 alias ga='git add'
