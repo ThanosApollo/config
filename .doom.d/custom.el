5ceafd867ba3b602d5129acd67dce0cafb1dea53
ThanosApollo <apollothanosmd@gmail.com>
2022-06-28 15:50:30 +0300

    Add linum configuration

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -20,3 +20,19 @@
  '(org-level-3 ((t (:inherit outline-2 :height 1.2))))
  '(org-level-4 ((t (:inherit outline-2 :height 1.0))))
  '(org-level-5 ((t (:inherit outline-2 :height 1.0)))))
+
+(defvar my-linum-current-line-number 0)
+
+(setq linum-format 'my-linum-relative-line-numbers)
+
+(defun my-linum-relative-line-numbers (line-number)
+  (let ((test2 (1+ (- line-number my-linum-current-line-number))))
+    (propertize
+     (number-to-string (cond ((<= test2 0) (1- test2))
+                             ((> test2 0) test2)))
+     'face 'linum)))
+
+(defadvice linum-update (around my-linum-update)
+  (let ((my-linum-current-line-number (line-number-at-pos)))
+    ad-do-it))
+(ad-activate 'linum-update)
