00dbb7a24c2b9e70b8cbd4cc49d4e5d88b6e0c28
Thanos Apollo <apollothanosmd@gmail.com>
2022-07-19 02:12:36 +0300

    Add RMS and remove reddit from elfeed

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -146,22 +146,18 @@
   :config
   (setq elfeed-search-feed-face ":foreground #fff :weight bold"
         elfeed-feeds (quote
-                       (("https://www.reddit.com/r/linux.rss" reddit linux)
-                        ("https://www.reddit.com/r/commandline.rss" reddit commandline)
-                        ("https://www.reddit.com/r/distrotube.rss" reddit distrotube)
-                        ("https://www.reddit.com/r/emacs.rss" reddit emacs)
-                        ("https://www.gamingonlinux.com/article_rss.php" gaming linux)
+                       (("https://www.gamingonlinux.com/article_rss.php" gaming linux)
                         ("https://hackaday.com/blog/feed/" hackaday linux)
                         ("https://opensource.com/feed" opensource linux)
                         ("https://linux.softpedia.com/backend.xml" softpedia linux)
                         ("https://www.thelancet.com/rssfeed/ebiom_current.xml" medicine ebiom)
-                        ("http://feeds.feedburner.com/d0od" omgubuntu linux)
                         ("https://www.computerworld.com/index.rss" computerworld linux)
                         ("https://www.networkworld.com/category/linux/index.rss" networkworld linux)
                         ("https://www.techrepublic.com/rssfeeds/topic/open-source/" techrepublic linux)
                         ("https://betanews.com/feed" betanews linux)
                         ("http://lxer.com/module/newswire/headlines.rss" lxer linux)
-                        ("https://distrowatch.com/news/dwd.xml" medicine lancet)))))
+                        ("https://distrowatch.com/news/dwd.xml" medicine lancet)
+                        ("https://stallman.org/rss/rss.xml"RichardStallman)))))
 
 (use-package elfeed-goodies
   :init
